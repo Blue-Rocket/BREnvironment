69c662808a9e45bc80fbf0e5ebe72bfd6589b451
Matt Magoffin <git+matt@msqr.us>
2014-04-29 17:02:49 +1200

    Fix license.

--- Environmental/EnvironmentTableViewController.m
+++ Environmental/EnvironmentTableViewController.m
@@ -3,7 +3,7 @@
 //  BREnvironment
 //
 //  Created by Matt on 4/29/14.
-//  Copyright (c) 2014 Blue Rocket, Inc. All rights reserved.
+//  Copyright (c) 2013 Blue Rocket, Inc. Distributable under the terms of the Apache License, Version 2.0.
 //
 
 #import "EnvironmentTableViewController.h"
